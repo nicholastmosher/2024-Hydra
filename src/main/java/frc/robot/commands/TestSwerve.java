b7ff12d55fc7598bf36a31bbea7bc9666dc9caa0
Shop <fkalmback@comcast.net>
2024-02-17 10:51:07 -0500

    Add DiagnoseSwerve command

---
+++ src/main/java/frc/robot/commands/TestSwerve.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Swerve;
+
+public class TestSwerve extends Command {
+    private final Swerve mSwerve;
+    private final double mX;
+    private final double mY;
+    private final double mRotation;
+    public TestSwerve(Swerve swerve, double x, double y, double rotation) {
+        mSwerve = swerve;
+        mX = x;
+        mY = y;
+        mRotation = rotation;
+    }
+
+    @Override
+    public void initialize() {
+        mSwerve.drive(new Translation2d(mX,mY), mRotation,false,true);
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        mSwerve.drive(new Translation2d(0,0),0, false, true);
+    }
+}
