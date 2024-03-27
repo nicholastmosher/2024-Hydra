905c5192e4bdf1b08d354a54a5e9333b7085df07
Marzoman5 <aemarzoratti@gmail.com>
2024-03-26 20:14:20 -0400

    arm constant

--- src/main/java/frc/lib/Constants.java
+++ src/main/java/frc/lib/Constants.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public final class Constants {
 
     public static IntakeConfig intakeConfig = new IntakeConfig(50, -0.8);
-    public static ArmConfig armConfig = new ArmConfig(51, 52, new PidConfig(1.0, 0.0, 0.0), 0.45, 0.45, 0.665, 1, 163.32+180, new DigitalInput(2));
+    public static ArmConfig armConfig = new ArmConfig(51, 52, new PidConfig(1.0, 0.0, 0.0), 0.45, 0.45, 0.663, 1, 163.32+180, new DigitalInput(2));
     public static ShooterConfig shooterConfig = new ShooterConfig(53, 54, -0.2, 10000);
     public static IndexerConfig indexerConfig = new IndexerConfig(55, -0.5, 0.15);
     public static LightConfig lightConfig = new LightConfig(0, 1, 1.5, 0);
